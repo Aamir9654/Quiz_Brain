19a095c372504c9019138db2c711971573fbb8cb
Aamir9654 <127832493+Aamir9654@users.noreply.github.com>
2024-09-14 20:43:37 +0530

    Add files via upload

---
+++ question_model.py
@@ -0,0 +1,11 @@
+class Question:
+
+    def __init__(self,q_text,q_answer,q_category):
+        self.text = q_text
+        self.answer = q_answer
+        self.category = q_category
+
+
+
+new_q = Question("aamir","False","q_category")
+
